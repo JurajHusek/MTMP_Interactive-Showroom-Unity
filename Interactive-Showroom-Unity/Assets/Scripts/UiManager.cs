dcc56fb98f45ae0f48f6f3eaf9ff65b30577003a
Juraj Hušek <juraj.husek@gmail.com>
2025-11-24 15:30:01 +0100

    v0.3

--- Interactive-Showroom-Unity/Assets/Scripts/UiManager.cs
+++ Interactive-Showroom-Unity/Assets/Scripts/UiManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class UiManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform ship1Object;
+    public float rotationSpeed = 20f;
+    private bool isRotatingShip1 = false;
+    public Transform ship2Object;
+    private bool isRotatingShip2 = false;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isRotatingShip1)
+        {
+            ship1Object.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
+        }
+        if (isRotatingShip2)
+        {
+            ship2Object.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
+        }
+    }
+    public void ActivateObject(GameObject obj)
+    {
+        obj.SetActive(true); 
+    }
+    public void DeactivateObject(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+    public void RotationShip1(bool status)
+    {
+        isRotatingShip1 = status;
+    }
+    public void RotationShip2(bool status)
+    {
+        isRotatingShip2 = status;
     }
 }
